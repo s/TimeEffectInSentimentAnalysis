63ca6d1e5e488c472a3b2a95afa2ca261fe749a2
Said <saidozcn@gmail.com>
2015-12-10 19:14:45 +0200

    Adds DB models

---
+++ models/TTNetTweet.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+#################################################
+# TTNetTweet.py  		
+# September 20
+# TweetRetriever
+#################################################
+
+from sqlalchemy import Column
+from sqlalchemy.types import *
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class TTNetTweet(Base):
+    __tablename__ = 'TTNetTweets'
+    id = Column(String(30), primary_key=True)
+    text = Column(String(512))
+    created_at = Column(TIMESTAMP())
+    lang = Column(String(5))
+    source = Column(String(128))
+    user_id = Column(String(30))
+    tweet_class = Column(Enum('positive', 'negative', 'neutral', name='tweet_class_type'))
+
+    def __init__(self):
+        pass
