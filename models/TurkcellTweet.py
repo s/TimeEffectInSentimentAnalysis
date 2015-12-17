98232ef830b06f546b682b60a5526a2d05ebb0b6
Said <saidozcn@gmail.com>
2015-12-17 22:09:57 +0200

    Fixes indentation

--- models/TurkcellTweet.py
+++ models/TurkcellTweet.py
@@ -12,16 +12,16 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class TurkcellTweet(Base):
+    __tablename__ = 'TurkcellTweets'
+    id = Column(String(30), primary_key=True)
+    text = Column(String(512))
+    created_at = Column(TIMESTAMP())
+    lang = Column(String(5))
+    source = Column(String(128))
+    user_id = Column(String(30))
+    tweet_class = Column(Enum('positive', 'negative', 'neutral', name='tweet_class_type'))
 
-	__tablename__ = 'TurkcellTweets'
-	id = Column(String(30), primary_key=True)
-	text =  Column(String(512))
-	created_at = Column(TIMESTAMP())
-	lang = Column(String(5))
-	source = Column(String(128))
-	user_id = Column(String(30))
-	tweet_class = Column(Enum('positive', 'negative', 'neutral', name='tweet_class_type'))
-
-	def __init__(self):
-		pass
+    def __init__(self):
+        pass
